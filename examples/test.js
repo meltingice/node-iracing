9cbd5d9bed9b1e9a43612621222b8603a66f27fd
meltingice <meltingice8917@gmail.com>
2013-01-22 20:29:39 -0500

    Add support for getting session object and track name

--- examples/test.js
+++ examples/test.js
@@ -2,5 +2,16 @@ var iRacing = require('../build/Release/iracing.node').iRacing;
 var ir;
 
 ir = new iRacing();
-console.log(ir);
-console.log(ir.waitForDataReady(60));
+
+console.log("Waiting for connection...");
+console.log("Press Ctrl+C to quit");
+
+while (true) {
+  if (ir.waitForDataReady(60)) {
+    console.log("Connection established!");
+    console.log(ir.getHeader());
+    console.log(ir.getTrack());
+    break;
+    //console.log(ir.getTelemetry());
+  }
+}
--- src/iracing.cc
+++ src/iracing.cc
@@ -6,6 +6,9 @@
 
 using namespace v8;
 
+char *data = NULL;
+int dataLen = 0;
+
 class IRacing: node::ObjectWrap {
 private:
 public:
@@ -23,7 +26,10 @@ public:
     IRacing::pft->InstanceTemplate()->SetInternalFieldCount(1);
     IRacing::pft->SetClassName(String::NewSymbol("IRacing"));
 
+    // Methods
     NODE_SET_PROTOTYPE_METHOD(IRacing::pft, "waitForDataReady", WaitForDataReady);
+    NODE_SET_PROTOTYPE_METHOD(IRacing::pft, "getHeader", GetHeader);
+    NODE_SET_PROTOTYPE_METHOD(IRacing::pft, "getTrack", GetTrack);
 
     target->Set(String::NewSymbol("iRacing"), IRacing::pft->GetFunction());
   }
@@ -31,21 +37,56 @@ public:
   static Handle<Value> New(const Arguments& args) {
     HandleScope scope;
 
-    IRacing* ir = new IRacing();
-    ir->Wrap(args.This());
+    IRacing* self = new IRacing();
+    self->Wrap(args.This());
     
-    return args.This();
+    return scope.Close(args.This());
   }
 
   static Handle<Value> WaitForDataReady(const Arguments& args) {
     HandleScope scope;
-    IRacing* inst = node::ObjectWrap::Unwrap<IRacing>(args.This());
+    //IRacing* self = node::ObjectWrap::Unwrap<IRacing>(args.This());
 
     String::Utf8Value data(args[1]->ToString());
     bool result = irsdk_waitForDataReady(args[0]->Int32Value(), *data);
-    return Boolean::New(result);
+    return scope.Close(Boolean::New(result));
   }
 
+  static Handle<Value> GetHeader(const Arguments& args) {
+    HandleScope scope;
+    //IRacing* inst = node::ObjectWrap::Unwrap<IRacing>(args.This());
+
+    const irsdk_header *header = irsdk_getHeader();
+
+    // if (!data || dataLen != header->bufLen) {
+    //   if (data) {
+    //     delete [] data;
+    //   }
+
+    //   char* data = new char[header->bufLen];
+    // }
+
+    Handle<Object> obj = Object::New();
+    obj->Set(String::New("ver"), Int32::New(header->ver));
+    obj->Set(String::New("status"), Int32::New(header->status));
+    obj->Set(String::New("tickRate"), Int32::New(header->tickRate));
+
+    return scope.Close(obj);
+  }
+
+  static Handle<Value> GetTrack(const Arguments& args) {
+    HandleScope scope;
+
+    const char* valueStr;
+    int valueLen;
+
+    if (parseYaml(irsdk_getSessionInfoStr(), "WeekendInfo:TrackName:", &valueStr, &valueLen)) {
+      char* track = new char[valueLen];
+      sprintf(track, "%.*s", valueLen, valueStr);
+
+      return scope.Close(String::New(track));
+    }
+  }
 };
 
 Persistent<FunctionTemplate> IRacing::pft;
